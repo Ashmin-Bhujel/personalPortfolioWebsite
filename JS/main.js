b4ee38a96e27265df7d3fdf7c4cfc31d4e300a91
Ashmin Bhujel <ashminbhujel01@gmail.com>
2022-03-07 09:55:22 +0545

    Added basic files of website

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+alert(`This Personal Portfolio Website is still being built and maintained by Ashmin Bhujel.
+Rotate or orient your device to landscape mode and enable desktop mode.
+
+Thank You!
+Be Happy, Safe and Creative`);
